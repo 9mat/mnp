76fd164768908c25e4e7909175d2e423e05e2bc9
Duong Hai Long <dhlong.hp@gmail.com>
2014-01-11 18:21:27 +0800

    modify LogLike.m, iterate over the groups
    not ready for different choice sets yet, but conformed to the case of only choice set

--- LogLike.m
+++ LogLike.m
@@ -4,24 +4,23 @@ n = dataR.n;
 
 if nargout == 1
     
-    probChosen  = ProbitProb( theta, dataR(1), n, spec );
+    nLogLike = 0;
+    for k = 1:numel(dataR)
+        probChosen  = ProbitProb( theta, dataR(k), n, spec );
+        probChosen( probChosen == 0 ) = eps;
+        nLogLike = nLogLike - sum(log(probChosen));
+    end
     
-    % Modify 0 values in 'probChosen' to machine epsilon to avoid log( 0 )
-    %   or division by 0  
-    probChosen( probChosen == 0 )   = eps;
-
-    nLogLike    = -sum( log( probChosen ) );
-
 elseif nargout > 1
-   
-    [ probChosen, d_probChosen ]    = ProbitProb( theta, dataR(1), n, spec );
+    nLogLike = 0;
+    d_nLogLike = zeros(size(theta));
     
-    % Modify 0 values in 'probChosen' to machine epsilon to avoid log( 0 )
-    %   or division by 0        
-    probChosen( probChosen == 0 )   = eps;
-        
-    nLogLike    = -sum( log( probChosen ) );
-    d_nLogLike  = -sum( bsxfun( @rdivide, d_probChosen, probChosen' ), 2 ); 
+    for k = 1:numel(dataR)
+        [ probChosen, d_probChosen ]    = ProbitProb( theta, dataR(1), n, spec );
+        probChosen( probChosen == 0 )   = eps;
+        nLogLike    = nLogLike-sum( log( probChosen ) );
+        d_nLogLike  = d_nLogLike-sum( bsxfun( @rdivide, d_probChosen, probChosen' ), 2 );
+    end
     
 end
 
