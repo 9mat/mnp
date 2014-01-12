797fa40fb8b3d2fb04f84aa26859a89bb7de9e75
Duong Hai Long <dhlong.hp@gmail.com>
2014-01-12 12:10:14 +0800

    fix bug to pick u s11 if it is not scaled

--- ConstructData.m
+++ ConstructData.m
@@ -43,19 +43,24 @@ mask.beta_2(spec.base,:) = 0;
 mask.beta_1(spec.base,:) = 0;
 mask.S(spec.base,:) = 0;
 mask.S(:,spec.base) = 0;
-spec.scale = 1 + (spec.base == 1); % if base = 1, scale = 2
+mask.S_noscale = mask.S;
 mask.S(spec.scale, spec.scale) = 0;
 
 mask.beta_1(mask.beta_1 == 1) = 1:sum(mask.beta_1(:));
 mask.beta_2(mask.beta_2(:) == 1) = 1:sum(mask.beta_2(:));
 mask.S(mask.S == 1) = 1:sum(mask.S(:));
+mask.S_noscale(mask.S_noscale == 1) = 1:sum(mask.S_noscale(:));
 
 for k = 1:n.choiceset
     missing = ~de2bi(uniquecode(k));
     
     pick.beta_1 = mask.beta_1;
     pick.beta_2 = mask.beta_2;
-    pick.S = mask.S;
+    if dataR(k).missingscale
+        pick.S = mask.S_noscale;
+    else
+        pick.S = mask.S;
+    end
     
     pick.beta_1(missing,:) = 0;
     pick.beta_2(missing,:) = 0;
